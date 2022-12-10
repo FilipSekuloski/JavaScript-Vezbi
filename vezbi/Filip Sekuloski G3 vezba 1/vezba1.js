ede47a035e784569e737d1cad42919a5d8312da6
FilipSekuloski <65288579+FilipSekuloski@users.noreply.github.com>
2022-12-10 13:52:46 +0100

    Add files via upload

---
+++ vezbi/Filip Sekuloski G3 vezba 1/vezba1.js
@@ -0,0 +1,7 @@
+let howManyPhones = 30;
+let phonePrice = 119.95;
+let calculate1 = phonePrice * (1+(5/100));
+let calculate2 = calculate1 * howManyPhones;
+console.log(calculate1);
+console.log(calculate2);
+
